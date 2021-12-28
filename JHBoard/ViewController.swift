22c39dfb246e91ae234be4a8d90a4ad8ef7711f4
kk_eezz <qowngus33@naver.com>
2021-12-28 09:21:35 +0900

    Initial Commit

---
+++ JHBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JHBoard
+//
+//  Created by 배주현 on 2021/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
